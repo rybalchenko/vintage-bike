d2ab7dfe48372887cd48cf4d17f149866c5d1041
rybalchenko <pavel3220@mail.ru>
2016-02-02 18:40:22 +0200

    first

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-;(function(){
+// ;(function(){
+// var menuToggle = document.getElementById("ba-menu-toggle"),
+// body=document.body;
 
-})();
+// menuToggle.onclick = function (event) {
+// 	event.preventDefault();
+// 	if ( body.className === "ba-menu-opened") {
+// 		body.className = "";
+// 		b
+// 	} else {
+// 		body.className = "ba-menu-opened";
+// 	}
+// }
+// console.log(menuToggle);
+// console.log(body);
+// })();
+
+;(function($){
+	$(function(){
+		var body = $('body'),
+		menuToggle = $('.ba-menu-toggle');
+
+		menuToggle.click(function(event){
+			event.preventDefault();
+			body.toggleClass('ba-menu-opened');
+		});
+	});
+})(jQuery);
